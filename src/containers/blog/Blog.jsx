f32c7be3dc2a07c56e8f67f6f4acf4d34d55aa44
Santu Ghosh <sghosh@nsight-inc.com>
2021-12-01 01:13:20 +0530

    first commit

---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './blog.css';
+
+const blog = () => {
+    return (
+        <div>
+            <h1>Blog</h1>
+        </div>
+    )
+}
+
+export default blog
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './whatgpt3.css';
+
+const WhatGPT3 = () => {
+    return (
+        <div className="App">
+           <h1>WhatGPT3</h1>
+            
+        </div>
+    )
+}
+
+export default WhatGPT3;
